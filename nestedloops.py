82e672815bd56fc735f37b54f7660825b17de7dd
Oscar Somba <maingioscar2@gmail.com>
2025-05-27 04:59:11 +0300

    Nestedloops.py

---
+++ nestedloops.py
@@ -0,0 +1,18 @@
+#nested loop = A loop within another loop either INNER or OUTER 
+#It can be:
+  #1. A for loop inside a for loop
+  #2. A while loop inside a while loop
+  #3. A while loop inside a for loop
+  #4. A for loop inside a while loop
+
+#rows = int(input("Enter the number of rows: "))
+#columns = int(input("Enter the number of columns: "))
+#symbol = input("Enter a symbol to use: ")
+#for x in range(rows):
+#You can set the numbers to appear on the same line
+ #for count in range(columns):
+   # print(symbol, end="")
+#print()#this will print them in line
+
+
+#COUNTDOWN TIMER PROGRAM
