f2493e295010e99ec074f0b9a98ee1f28be9bd40
Oscar Somba <maingioscar2@gmail.com>
2025-12-03 17:02:34 +0300

    3rd December 2025

--- PYTHON/randomnumbers.py
+++ PYTHON/randomnumbers.py
@@ -1,46 +1,81 @@
 import random
 
-low = 1
-high = 100
-options = ("rock", "paper", "scissors")
-cards = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]
-# number = random.randint(1, 20) #prints out different integers through the range of 1 and 6 each time it is executed e.g roller dice program
-number = random.randint(low, high) #prints out different integers through the range of the variables low and high
-number = random.random() # prints out floating point between 0 and 1
-#.random() takes no arguments like .randint which can take 2 arguments
-option = random.choice(cards) #great for picking a random element
-random.shuffle(cards) #shuffling(changing in between the values)
-#works with objects only 
-# print(cards)
-# print(option)
+# low = 1
+# high = 100
+# options = ("rock", "paper", "scissors")
+# cards = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]
+# # number = random.randint(1, 20) #prints out different integers through the range of 1 and 6 each time it is executed e.g roller dice program
+# number = random.randint(low, high) #prints out different integers through the range of the variables low and high
+# number = random.random() # prints out floating point between 0 and 1
+# #.random() takes no arguments like .randint which can take 2 arguments
+# option = random.choice(cards) #great for picking a random element
+# random.shuffle(cards) #shuffling(changing in between the values)
+# #works with objects only 
+# # print(cards)
+# # print(option)
 
-#PYTHON NUMBER GUESSING GAME 
-lowest_num = 1
-highest_num = 100
-#Setting the range in terms of variables
-answer = random.randint(lowest_num, highest_num)#lowest_num and highest_num are the arguments
-guesses = 0
-is_running = True
-print("----Welcome to Oscar Python Number Guessing Game-----")
-print(f"Select a number between {lowest_num} and {highest_num}: ")
+# #1. PYTHON NUMBER GUESSING GAME 
+# lowest_num = 1
+# highest_num = 100
+# #Setting the range in terms of variables
+# answer = random.randint(lowest_num, highest_num)#lowest_num and highest_num are the arguments
+# guesses = 0
+# is_running = True
+# print("----Welcome to Oscar Python Number Guessing Game-----")
+# print(f"Select a number between {lowest_num} and {highest_num}: ")
 
-while is_running:
-    guess = input("Enter your guess: ")
-    if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
+# while is_running:   
+#     guess = input("Enter your guess: ")
+#     if guess.isdigit():
+#         guess = int(guess)
+#         guesses += 1
         
-        if guess < lowest_num or guess > highest_num:
-            print("That number is out of range")
-            print(f"Please select a number between {lowest_num} and {highest_num}: ")
-        elif guess < answer:
-            print("Too low! Try again!")
-        elif guess > answer:
-            print("Too high! Try again!")
-        else:
-            print(f"CORRECT! The answer is {guess}")
-            print(f"Number of guesses: {guesses}")
-            is_running = False
+#         if guess < lowest_num or guess > highest_num:
+#             print("That number is out of range")
+#             print(f"Please select a number between {lowest_num} and {highest_num}: ")
+#         elif guess < answer:
+#             print("Too low! Try again!")
+#         elif guess > answer:
+#             print("Too high! Try again!")
+#         else:
+#             print(f"CORRECT! The answer is {guess}")
+#             print(f"Number of guesses: {guesses}")
+#             is_running = False
+#     else:
+#         print("Invalid guess")
+#         print(f"Please select a number between {lowest_num} and {highest_num}: ")
+
+# 2. ROCK, PAPER, SCISSORS GAME
+options = ("rock", "paper", "scissors")
+player = None
+computer = random.choice(options)
+
+running = True
+while running:
+    player = None
+    computer = random.choice(options)
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and  computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
     else:
-        print("Invalid guess")
-        print(f"Please select a number between {lowest_num} and {highest_num}: ")
+        print("You lose!")
+    # play_again = input("You wanna play again?(y/n):").lower()
+    # if play_again == "n":
+    #     running = False
+    if  input("Would you like to play again?(y,n): ").lower() == "n":
+        running = False
+print("Thanks for playing!")
+
+#3. ROLLER DICE PROGRAM
