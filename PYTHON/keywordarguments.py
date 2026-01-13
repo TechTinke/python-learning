6c0bc7c035346da6021e80618ef9964ce1c8e19d
Oscar Somba <maingioscar2@gmail.com>
2026-01-13 08:17:38 +0300

    Keyword arguments, 13th January 2026

--- PYTHON/keywordarguments.py
+++ PYTHON/keywordarguments.py
@@ -16,5 +16,18 @@ def hello(greeting, title, first, last):
     return greeting + " " + title + " " + first + " " + last
 
 
-greeting = hello("morning", "mrs", "annah", "somba")
+greeting = hello("morning", "mrs", "annah", "somba")#Positional argument should come before any keyword argument
 print(greeting)
+#keyword arguments are used to improve readability
+
+#NB: Some of the built in functions such as print have keyword arguments for example
+for x in range(1,11):
+    print (x, end=" ")#end is a keyword argument found within the built in print statement
+print(" ")
+print("1", "2", "3", "4", "5", sep="-")#sep is a keyword argument found within the built in print statement
+
+
+def get_phone(country, area, first, last):
+    return f"{country}-{area}-{first}-{last}"
+phone_num = get_phone(country=254, area=7, first=9049, last=9672)
+print(phone_num)
