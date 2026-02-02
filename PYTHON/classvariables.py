f6a6c2c02a35bc96ec6730fc2d688138e97244f3
Oscar Somba <maingioscar2@gmail.com>
2026-02-02 19:09:27 +0300

    Class Variables, 2nd February 2026

--- PYTHON/classvariables.py
+++ PYTHON/classvariables.py
@@ -17,12 +17,12 @@ student2 = Student("Patrick", 35)
 student3 = Student("Squidward", 55)
 student4 = Student("Sandy", 27)
 
-# print(student1.name)
-# print(student1.age)
-# print(student2.name)
-# print(student2.age)
-# print(Student.class_year)#Improved readability that class_year is a class variable not instance
-# print(Student.num_students)
+print(student1.name)
+print(student1.age)
+print(student2.name)
+print(student2.age)
+print(Student.class_year)#Improved readability that class_year is a class variable not instance
+print(Student.num_students)
 
 print(f"My graduating class of {Student.class_year} has {Student.num_students} students")
 print(student1.name)
