5166e67ba9437a5aafada08f008fc04fadaeb559
Oscar Somba <maingioscar2@gmail.com>
2026-02-14 15:07:48 +0300

    Duck Typing, 14th February 2026

---
+++ PYTHON/ducktyping.py
@@ -0,0 +1,28 @@
+# "Duck Typing" = Another way to achieve polymorphism besides Inheritance
+#                 Object must have the minimum necessary attributes/methods
+#                 "If it looks like a duck and quacks like a duck, it must be a duck"
+
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
+
+class Car:
+    alive = False
+    def speak(self):
+        print("HONK!")
+
+animals = [Dog(), Cat(), Car()] #Car meets the minimum requirements to be called an Animal(has speak attribute, alive attribute)
+#                               even though it doesn't inherit from the class Animal                                
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
+
