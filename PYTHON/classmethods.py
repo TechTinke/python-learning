fa895357acd4bf329f712b34f6b78f9af1685aa4
Oscar Somba <maingioscar2@gmail.com>
2026-02-15 21:03:13 +0300

    Class Methods, 15th January 2026

--- PYTHON/classmethods.py
+++ PYTHON/classmethods.py
@@ -4,19 +4,35 @@
 class Student:
 
     count = 0
+    total_gpa = 0
 
     def __init__(self, name, gpa):
         self.name = name
         self.gpa = gpa
         Student.count += 1 #Increase count by 1 for every student that is created
+        Student.total_gpa += gpa
     
     #Instance Method
     def get_info(self):
         return f"{self.name} {self.gpa}"
     
-    #Class Method
+    #Class Method 1
     @classmethod
     def get_count(cls):
         return f"Total no of students: {cls.count}"
+    
+    #Class Method 2
+    @classmethod
+    def get_average_gpa(cls):
+        if cls.count == 0:
+            return 0
+        else:
+            return f"Average gpa: {cls.total_gpa / cls.count:.2f}"
+    
+    
+student1 = Student("Spongebob", 3.2)
+student2 = Student("Patrick", 2.0)
+student3 = Student("Sandy", 4.0)
 
 print(Student.get_count())
+print(Student.get_average_gpa())
