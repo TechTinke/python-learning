1b1e997935fa0c88d0ff05c0af688159343d0a3a
Oscar Somba <maingioscar2@gmail.com>
2026-02-15 21:09:16 +0300

    Magic Methods, 15th February 2026

---
+++ PYTHON/magicmethods.py
@@ -0,0 +1,3 @@
+# Magic Methods = Dunder methods (double underscore) __init__, __str__, __eq__
+#                 They are automatically called by many of Python's built-in operations
+#                 They allow developers to define or customize the behaviour of objects
