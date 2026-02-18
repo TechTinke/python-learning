3f047c8fdb6eea300734b30a3ed3e2fce37d9f2f
Oscar Somba <maingioscar2@gmail.com>
2026-02-19 00:41:05 +0300

    Reading files, 19th February 2026

--- PYTHON/readingfiles.py
+++ PYTHON/readingfiles.py
@@ -1,9 +1,21 @@
 #Python reading files(.txt, .json, .csv)
 
-file_path = "PYTHON/output.txt"
+import json #importing the json module
+import csv #importing the csv module
 
+# file_path = "PYTHON/output.tx"
+    
+file_path = "PYTHON/output.json"
 #with statement is used to wrap a block of code within a context manager
-with open(file=file_path, mode="r") as file:
-    content = file.read()
-    print(content)
+
+try:
+    with open(file=file_path, mode="r") as file:
+        # content = file.read() #.txt file
+        content = json.load(file)
+        print(content["name"])
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have the permission to read this file")
+
 
