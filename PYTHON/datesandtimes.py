437e7be92fcb5184de3dcef38e2c57897827b1b0
Oscar Somba <maingioscar2@gmail.com>
2026-02-19 01:16:40 +0300

    Introduction to dates and times, 19th February 2026

---
+++ PYTHON/datesandtimes.py
@@ -0,0 +1,13 @@
+import datetime  #helps us with date and time modules
+
+date = datetime.date(2025, 1, 2)
+today = datetime.date.today()
+
+time = datetime.time(12,30,0)
+now =  datetime.datetime.now()
+
+
+print(date)
+print(today)
+print(time)
+print(now)
