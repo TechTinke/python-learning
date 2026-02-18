cb3aad16d4cec3518add1a296711907b820f9f25
Oscar Somba <maingioscar2@gmail.com>
2026-02-19 00:55:27 +0300

    Reading files, 19th February 2026

--- PYTHON/readingfiles.py
+++ PYTHON/readingfiles.py
@@ -3,16 +3,18 @@
 import json #importing the json module
 import csv #importing the csv module
 
-# file_path = "PYTHON/output.tx"
-    
-file_path = "PYTHON/output.json"
-#with statement is used to wrap a block of code within a context manager
+# file_path = "PYTHON/output.txt"
+# file_path = "PYTHON/output.json"
+file_path = "PYTHON/output.csv"
 
+#with statement is used to wrap a block of code within a context manager
 try:
     with open(file=file_path, mode="r") as file:
         # content = file.read() #.txt file
-        content = json.load(file)
-        print(content["name"])
+        # content = json.load(file) # .json file
+        content = csv.reader(file) # .csv file but this returns a memory address
+        for line in content:
+            print(line[2])
 except FileNotFoundError:
     print("That file was not found")
 except PermissionError:
