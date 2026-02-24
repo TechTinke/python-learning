82698331b9a1fdc0badccd9edfee3ca10470daf7
Oscar Somba <maingioscar2@gmail.com>
2026-02-24 17:20:28 +0300

    Requesting API data, 22nd February 2026

--- PYTHON/requestapidata.py
+++ PYTHON/requestapidata.py
@@ -1,29 +1,29 @@
-# How to connect to an API using Python
+# # How to connect to an API using Python
 
-import requests # Import the requests modules for requests
+# import requests # Import the requests modules for requests
 
-base_url = "https://pokeapi.co/api/v2"
+# base_url = "https://pokeapi.co/api/v2"
 
-def get_pokemon_info(name):
-    url = f"{base_url}/pokemon/{name}"
-    response = requests.get(url)
-    print(response) # prints out the status code
+# def get_pokemon_info(name):
+#     url = f"{base_url}/pokemon/{name}"
+#     response = requests.get(url)
+#     print(response) # prints out the status code
     
-    if response.status_code == 200:
-        pokemon_data = response.json()
-        print("Data retrieved!")
-        return pokemon_data
-    else:
-        print(f"Failed to retrieve data {response.status_code}")
+#     if response.status_code == 200:
+#         pokemon_data = response.json()
+#         print("Data retrieved!")
+#         return pokemon_data
+#     else:
+#         print(f"Failed to retrieve data {response.status_code}")
 
-pokemon_name = "Typhlosion"
-pokemon_info = get_pokemon_info(pokemon_name)
+# pokemon_name = "Typhlosion"
+# pokemon_info = get_pokemon_info(pokemon_name)
 
-if pokemon_info:
-    print(f"Name: {pokemon_info['name'].capitalize()}")
-    print(f"Id: {pokemon_info['id']}")
-    print(f"Height: {pokemon_info['height']}")
-    print(f"Weight: {pokemon_info['weight']}")
+# if pokemon_info:
+#     print(f"Name: {pokemon_info['name'].capitalize()}")
+#     print(f"Id: {pokemon_info['id']}")
+#     print(f"Height: {pokemon_info['height']}")
+#     print(f"Weight: {pokemon_info['weight']}")
 
 # STATUS CODES
 # 1. 200 - response was okay
@@ -36,6 +36,34 @@ if pokemon_info:
 # Prints temperature, wind speed, weather code if 200
 # Handles non-200 status with error message
 # Test with Nairobi coordinates (lat ≈ -1.286, lon ≈ 36.817).
+
+import requests
+
+base_url = "https://api.open-meteo.com/v1/forecast"
+
+def get_weather(lat, lon):
+    lat_url = f"{base_url}/{lat}"
+    lon_url = f"{base_url}/{lon}"
+    lat_response = requests.get(lat_url)
+    lon_response = requests.get(lon_url)
+
+    if lat_response.status_code == 200:
+        print("Data has been retrieved successfully")
+        lat_data = lat_response.json()
+        return lat_data
+    else:
+        print("Unable to retrieve latitude data {lat_response.status_code}")
+    
+    if lon_response.status_code == 200:
+        print("Data has been retrieved successfully")
+        lon_data = lon_response.json()
+        return lon_data
+    else:
+        print("Unable to retrieve longitude data {lon_response.status_code}")
+
+weather_data = get_weather(-1.286, 36.817)
+
+
 
 # 2. GitHub User Info
 # Create get_github_user(username: str):
