2c732783869af438993e61f5b73c9e5d24fe1422
Oscar Somba <maingioscar2@gmail.com>
2026-03-17 23:02:58 +0300

    Class Methods - App Configuration, 17th March 2026

--- PYTHON/classmethods.py
+++ PYTHON/classmethods.py
@@ -2,38 +2,41 @@
 #                 Take (cls) as the first parameter, which represents the class itself
 #                 Best for class level data or require access to the class itself
 
-class Student:
+# Class Methods deal with class level data such as class variables or behaviour
+# Instance  Methods deal with individual instances(objects) belonging to a class
 
-    count = 0
-    total_gpa = 0
+# class Student:
 
-    def __init__(self, name, gpa):
-        self.name = name
-        self.gpa = gpa
-        Student.count += 1 #Increase count by 1 for every student that is created
-        Student.total_gpa += gpa
+#     count = 0
+#     total_gpa = 0
+
+#     def __init__(self, name, gpa):
+#         self.name = name
+#         self.gpa = gpa
+#         Student.count += 1 #Increase count by 1 for every student that is created
+#         Student.total_gpa += gpa
     
-    #Instance Method
-    def get_info(self):
-        return f"{self.name} {self.gpa}"
+#     #Instance Method
+#     def get_info(self):
+#         return f"{self.name} {self.gpa}"
     
-    #Class Method 1
-    @classmethod
-    def get_count(cls):
-        return f"Total no of students: {cls.count}"
+#     #Class Method 1
+#     @classmethod
+#     def get_count(cls):
+#         return f"Total no of students: {cls.count}"
     
-    #Class Method 2
-    @classmethod
-    def get_average_gpa(cls):
-        if cls.count == 0:
-            return 0
-        else:
-            return f"Average gpa: {cls.total_gpa / cls.count:.2f}"
+#     #Class Method 2
+#     @classmethod
+#     def get_average_gpa(cls):
+#         if cls.count == 0:
+#             return 0
+#         else:
+#             return f"Average gpa: {cls.total_gpa / cls.count:.2f}"
     
     
-student1 = Student("Spongebob", 3.2)
-student2 = Student("Patrick", 2.0)
-student3 = Student("Sandy", 4.0)
+# student1 = Student("Spongebob", 3.2)
+# student2 = Student("Patrick", 2.0)
+# student3 = Student("Sandy", 4.0)
 
 # print(Student.get_count())
 # print(Student.get_average_gpa())
@@ -110,16 +113,16 @@ student3 = Student("Sandy", 4.0)
 # and a class method set_theme(cls, new_theme) that updates the theme for the entire app. 
 # Why is a class method more appropriate here than an instance method?
 
-class AppSettings():
-    theme = "light"
+# class AppSettings():
+#     theme = "light"
 
-    @classmethod
-    def set_theme(cls, new_theme):
-        cls.theme = new_theme
+#     @classmethod
+#     def set_theme(cls, new_theme):
+#         cls.theme = new_theme
 
-print(AppSettings.theme)
-AppSettings.set_theme("dark")
-print(AppSettings.theme)
+# print(AppSettings.theme)
+# AppSettings.set_theme("dark")
+# print(AppSettings.theme)
 
 
 
