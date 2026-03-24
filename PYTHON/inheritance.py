36fe19bfdf4b54f205050aefeb5088e401d8456d
Oscar Somba <maingioscar2@gmail.com>
2026-03-24 07:33:16 +0300

    Inheritance, 24th March 2026

--- PYTHON/inheritance.py
+++ PYTHON/inheritance.py
@@ -6,47 +6,47 @@
 #The child classes can have different attributes(variables) 
 # and methods(functions) from the parent class 
 
-class Animal:
-    def __init__(self, name):
-        self.name = name
-        self.is_alive = True #Since it is set to True, it doesn't need to be set as a parameter
+# class Animal():
+#     def __init__(self, name):
+#         self.name = name
+#         self.is_alive = True #Since it is set to True, it doesn't need to be set as a parameter
     
-    def eat(self):
-        print(f"{self.name} is eating")
+#     def eat(self):
+#         print(f"{self.name} is eating")
     
-    def sleep(self):
-        print(f"{self.name} is sleeping")
+#     def sleep(self):
+#         print(f"{self.name} is sleeping")
 
-class Dog(Animal):
-    def speak(self):
-        print(f"{self.name} is barking: Woof!Woof!")
+# class Dog(Animal):
+#     def speak(self):
+#         print(f"{self.name} is barking: Woof!Woof!")
 
-class Cat(Animal):
-    def speak(self):
-        print(f"{self.name} is meowing!")
+# class Cat(Animal):
+#     def speak(self):
+#         print(f"{self.name} is meowing!")
 
-class Mouse(Animal):
-    def speak(self):
-        print(f"{self.name} is squeaking!")
+# class Mouse(Animal):
+#     def speak(self):
+#         print(f"{self.name} is squeaking!")
 
-dog = Dog("Scooby")
-cat = Cat("Garfield")
-mouse = Mouse("Mickey")
+# dog = Dog("Scooby")
+# cat = Cat("Garfield")
+# mouse = Mouse("Mickey")
 
-print(dog.name)
-print(cat.name)
-print(mouse.name)
-dog.eat()
-dog.sleep()
-cat.eat()
-cat.sleep()
-mouse.eat()
-mouse.sleep()
-dog.speak()
-cat.speak()
-mouse.speak()
+# print(dog.name)
+# print(cat.name)
+# print(mouse.name)
+# dog.eat()
+# dog.sleep()
+# cat.eat()
+# cat.sleep()
+# mouse.eat()
+# mouse.sleep()
+# dog.speak()
+# cat.speak()
+# mouse.speak()
 
-#WORK TO BE DONE
+# #WORK TO BE DONE
 # Banking System – Account Types
 # Create a base class BankAccount with:
 # __init__(self, account_number, holder, balance=0)
