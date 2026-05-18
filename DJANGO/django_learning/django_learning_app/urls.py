7dd758959da15d9836382df80c35a32d3544a87e
Oscar Somba <maingioscar2@gmail.com>
2026-05-18 12:01:36 +0300

    Building a word counter using Django

--- DJANGO/django_learning/django_learning_app/urls.py
+++ DJANGO/django_learning/django_learning_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index') # configure each URL, you add more URLs from here
+    path('', views.index, name='index'),# configure each URL, you add more URLs from here
+    path('counter', views.counter, name='counter')
 ]
--- DJANGO/django_learning/django_learning_app/views.py
+++ DJANGO/django_learning/django_learning_app/views.py
@@ -3,12 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request): #whatever is done to this function is what will be assigned to the URL
-    name = 'Erick'
-    # name = user.name
+    # name = 'Erick'
+    # # name = user.name
 
-    context = {
-        'name': 'Erick',
-        'age': 19,
-        'nationality': 'British'
-    }
-    return render(request, 'index.html', context)
+    # context = {
+    #     'name': 'Erick',
+    #     'age': 19,
+    #     'nationality': 'British'
+    # }
+    return render(request, 'index.html')
+
+def counter(request):
+    input_text = request.GET['input_text']
+    amount_of_words = len(input_text.split())
+    return render(request, 'counter.html', {'amount_of_words': amount_of_words})
