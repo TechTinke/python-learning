b3cfe4e23383fe7a9b1df5f793b4ce06b4aceefc
Oscar Somba <maingioscar2@gmail.com>
2026-05-18 10:10:44 +0300

    Sending data to a template file

--- DJANGO/django_learning/django_learning_app/views.py
+++ DJANGO/django_learning/django_learning_app/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request): #whatever is done to this function is what will be assigned to the URL
-    return render(request, 'index.html')
+    name = 'Erick'
+    # name = user.name
+
+    context = {
+        'name': 'Erick',
+        'age': 19,
+        'nationality': 'British'
+    }
+    return render(request, 'index.html', context)
