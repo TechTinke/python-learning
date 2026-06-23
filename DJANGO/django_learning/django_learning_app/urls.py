008a72d95887fc8a1a773b248703a39be3348f1d
Oscar Somba <maingioscar2@gmail.com>
2026-06-24 00:38:59 +0300

    Re-defined the URL paths for Listed Posts, Post Detail and Delete

--- DJANGO/django_learning/django_learning_app/urls.py
+++ DJANGO/django_learning/django_learning_app/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
+#NB:
+# - If URL refers to a specific object in your database, the URL Path 
+# should describe the object you are referrring to and not the verb
+
+# path('post/<str:pk>', views.post, name='post')
+
+# - If URL refers to a standalone action with no associated object from database
+# then the URL path can be named after the action
+
+# path('register', views.register, name='register')
 
 urlpatterns = [
     path('', views.index, name='index'),# configure each URL, you add more URLs from here
@@ -9,4 +19,4 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('counter', views.counter, name='counter'),
     path('post/<str:pk>', views.post, name='post') #string variable named pk
-]
+]   
--- DJANGO/django_learning/lost_and_found/urls.py
+++ DJANGO/django_learning/lost_and_found/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listed_posts', views.listed_posts, name='listed_posts'),
+    path('', views.listed_posts, name='listed_posts'),
     path('create_post', views.create_post, name='create_post'),
-    path('post_detail<str:pk>', views.post_detail, name='post_detail'),
-    path('delete_post<str:pk>', views.delete_post, name='delete_post' )
+    path('post/<str:pk>', views.post_detail, name='post_detail'),
+    path('post/<str:pk>/delete', views.delete_post, name='delete_post' )
 ]
