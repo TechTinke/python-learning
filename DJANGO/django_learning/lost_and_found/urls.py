eaae1340d77cb2719983b84bdfae8d44e8379cff
Oscar Somba <maingioscar2@gmail.com>
2026-06-23 08:24:49 +0300

    Defined the urls for the views

--- DJANGO/django_learning/lost_and_found/urls.py
+++ DJANGO/django_learning/lost_and_found/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index')
+    path('listed_posts', views.listed_posts, name='listed_posts'),
+    path('create_post', views.create_post, name='create_post'),
+    path('post_detail<str:pk>', views.post_detail, name='post_detail'),
+    path('delete_post<str:pk>', views.delete_post, name='delete_post' )
 ]
