67df850408c76ce86929eb3e8b301a016faa5006
Oscar Somba <maingioscar2@gmail.com>
2026-06-23 15:11:41 +0300

    Saved the views created earlier

--- DJANGO/django_learning/lost_and_found/views.py
+++ DJANGO/django_learning/lost_and_found/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 # Create your views here.
 def listed_posts(request):
-    listed_posts = Post.objects.all()
+    listed_posts = Post.objects.all() # getting all the posts that have been listed
     return render(request, 'lost_and_found/listed_posts.html', {'listed_posts': listed_posts})
 
 def post_detail(request, pk):
