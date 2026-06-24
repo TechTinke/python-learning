8750ad16353ee316653492ba9d8bab7a7ddf45ed
Oscar Somba <maingioscar2@gmail.com>
2026-06-24 09:46:16 +0300

    Redefined the URLs

--- DJANGO/django_learning/lost_and_found/urls.py
+++ DJANGO/django_learning/lost_and_found/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listed_posts, name='listed_posts'),
+    path('', views.listed_posts, name='listed_posts'), # gateway for lost_and_found
     path('create_post', views.create_post, name='create_post'),
-    path('post/<str:pk>', views.post_detail, name='post_detail'),
+    path('post/<str:pk>', views.post_detail, name='post_detail'), # post/<str:pk> - points to the particular object(post)
     path('post/<str:pk>/delete', views.delete_post, name='delete_post' )
 ]
