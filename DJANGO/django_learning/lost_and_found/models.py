4feeaf254c4150ff9805cdc1b3fd235d024f9c57
Oscar Somba <maingioscar2@gmail.com>
2026-06-25 14:01:11 +0300

    Applied migrations for the new field of owner and set default value to user_id 1

--- DJANGO/django_learning/lost_and_found/models.py
+++ DJANGO/django_learning/lost_and_found/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# NB:
+# - Every field that is created with no null=True is a required field (non-nullable)
+
+# - In cases when a new field has been created and that field is non-nullable 
+# and the other fields have already been populated, an error is thrown and you
+# have to set a default value from the terminal or set the default value manually from the model creation
+
 class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) # every Post row must have a real owner value
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
