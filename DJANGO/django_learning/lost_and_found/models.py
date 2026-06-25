37eb11ffed9f583531687717f42862fb1fbffc14
Oscar Somba <maingioscar2@gmail.com>
2026-06-25 15:31:43 +0300

    Anonymous User Logic notes

--- DJANGO/django_learning/lost_and_found/models.py
+++ DJANGO/django_learning/lost_and_found/models.py
@@ -9,6 +9,8 @@ from django.contrib.auth.models import User
 # and the other fields have already been populated, an error is thrown and you
 # have to set a default value from the terminal or set the default value manually from the model creation
 
+# - A user who is not logged in is specially assigned as Anonymous User by Django
+# but user.is_authenticated returns False when run
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE) # every Post row must have a real owner value
     title = models.CharField(max_length=100)
