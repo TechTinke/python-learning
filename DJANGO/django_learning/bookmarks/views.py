95962b2c9f0cee248b80637d6f7ad464181eb924
Oscar Somba <maingioscar2@gmail.com>
2026-06-28 16:44:14 +0300

    Defined two views only(saved_links & create_link with a pass

--- DJANGO/django_learning/bookmarks/views.py
+++ DJANGO/django_learning/bookmarks/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    pass
+def saved_links(request):
+    pass
+def create_link(request):
+    pass
