eee2989634159f9b34e249affb5e82801f4516fe
Oscar Somba <maingioscar2@gmail.com>
2026-06-28 17:18:22 +0300

    Defined all urls for bookmarks app

--- DJANGO/django_learning/bookmarks/urls.py
+++ DJANGO/django_learning/bookmarks/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'), #gateway for bookmarks app(login)
     path('saved_links', views.saved_links, name='saved_links'),
     path('create_link', views.create_link, name='create_link'),
+    path('link/<str:pk>', views.link_detail, name='link_detail'),
+    path('link/<str:pk/update', views.link_update, name='link_update')
 ]
