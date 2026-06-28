97c5f0e6a61ed321a3de0bfc5e83062511ef3109
Oscar Somba <maingioscar2@gmail.com>
2026-06-28 16:43:12 +0300

    Created the fields for Link model and added link_owner ForeignKey

--- DJANGO/django_learning/bookmarks/models.py
+++ DJANGO/django_learning/bookmarks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Link(models.Model):
+    link_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField(max_length=250)
+    details = models.CharField(max_length=300)
