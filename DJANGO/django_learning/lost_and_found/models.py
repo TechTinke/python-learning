b9f881c3ac0f12aedca76628e9726473d890b242
Oscar Somba <maingioscar2@gmail.com>
2026-06-28 09:04:27 +0300

    Extra notes

--- DJANGO/django_learning/lost_and_found/models.py
+++ DJANGO/django_learning/lost_and_found/models.py
@@ -6,12 +6,14 @@ from django.contrib.auth.models import User
 # - Every field that is created with no null=True is a required field (non-nullable)
 
 # - In cases when a new field has been created and that field is non-nullable 
-# and the other fields have already been populated, an error is thrown and you
+# and the other fields have already been populated like for example in this case owner
+# was created after title and descripiton were already defined,
+#  an error is thrown and you
 # have to set a default value from the terminal or set the default value manually from the model creation
 
 # - A user who is not logged in is specially assigned as Anonymous User by Django
 # but user.is_authenticated returns False when run
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE) # every Post row must have a real owner value
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100) #CharField is for characters
     description = models.CharField(max_length=500)
