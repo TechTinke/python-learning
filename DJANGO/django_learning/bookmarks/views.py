c435ec5c42e7bca8c38a36daba7b0de724554179
Oscar Somba <maingioscar2@gmail.com>
2026-06-29 12:01:26 +0300

    Defined the login view with user login authentication and redirection to the user's saved links onc e a user is logged in

--- DJANGO/django_learning/bookmarks/views.py
+++ DJANGO/django_learning/bookmarks/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from .models import Link
 # Create your views here.
-def index(request):
-    pass
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('bookmarks/saved_links')
+        else:
+            messages.info(request, 'OOPS! Your lgoin credentials could not be found')
+            return redirect('login')
+    return render(request, 'django_learning_app/login.html')
 def saved_links(request):
-    pass
+    saved_links = Link.objects.all()
+    return render(request, 'bookmarks/saved_links.html', {'saved_links': saved_links})
 def create_link(request):
-    pass
+    return render(request, 'bookmarks/create_link.html')
 def link_update(request, pk):
-    pass
+    return render(request, 'bookmarks/update_link.html', {'pk': pk})
 def link_detail(request, pk):
-    pass
+    return render(request, 'bookmarks/link_detail.html', {'pk': pk})
