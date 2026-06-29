7cd21f91d9239697e08aad0ed7ec3a2dc621c7e6
Oscar Somba <maingioscar2@gmail.com>
2026-06-29 11:58:16 +0300

    Changed the index url view to login instead of index

--- DJANGO/django_learning/bookmarks/urls.py
+++ DJANGO/django_learning/bookmarks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), #gateway for bookmarks app(login)
+    path('', views.index, name='login'), #gateway for bookmarks app(login)
     path('saved_links', views.saved_links, name='saved_links'),
     path('create_link', views.create_link, name='create_link'),
     path('link/<str:pk>', views.link_detail, name='link_detail'),
