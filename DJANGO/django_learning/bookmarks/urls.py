2c72c1e5e24fd9c264dbc1351892dafe677c8828
Oscar Somba <maingioscar2@gmail.com>
2026-07-01 21:40:36 +0300

    Added some notes on how django links work

--- DJANGO/django_learning/bookmarks/urls.py
+++ DJANGO/django_learning/bookmarks/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+#NB:
+# - URL names in Django live in one flat, project wide lookup table and
+# doesn't care what app is being pointed to
+# - That means a view from django_learning_app could be used in the bookmarks apps 
+# unless there is redirection that is app specific
+
 urlpatterns = [
     path('', views.login, name='login'), #gateway for bookmarks app(login)
     path('saved_links', views.saved_links, name='saved_links'),
