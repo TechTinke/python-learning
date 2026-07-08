b234af21831bfa5789c475ab9735a95b1e8ba6ce
Oscar Somba <maingioscar2@gmail.com>
2026-07-08 21:17:16 +0300

    Fixed the url path for update_link

--- DJANGO/django_learning/bookmarks/urls.py
+++ DJANGO/django_learning/bookmarks/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('saved_links', views.saved_links, name='saved_links'),
     path('create_link', views.create_link, name='create_link'),
     path('link/<str:pk>', views.link_detail, name='link_detail'),
-    path('link/<str:pk/update', views.link_update, name='link_update')
+    path('link/<str:pk>/update', views.update_link, name='update_link')
 ]
