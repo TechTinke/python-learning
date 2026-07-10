8eda9b3d213294c211cd3b3389d89c36ae4ece60
Oscar Somba <maingioscar2@gmail.com>
2026-07-10 09:51:30 +0300

    Pycache changes

--- DJANGO/django_learning/django_learning/urls.py
+++ DJANGO/django_learning/django_learning/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('django_learning_app/', include('django_learning_app.urls')),
     path('lost_and_found/', include('lost_and_found.urls')),
     path('bookmarks/', include('bookmarks.urls')),
-    path('/journal', include('journal.urls'))
+    path('journal/', include('journal.urls'))
 ]
