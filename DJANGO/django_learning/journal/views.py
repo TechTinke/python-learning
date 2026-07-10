6b6bb562be9662b733ab068dba101ddf8d10b695
Oscar Somba <maingioscar2@gmail.com>
2026-07-10 09:49:27 +0300

    Created the boilerplate for all the views with a pass in each view

--- DJANGO/django_learning/journal/views.py
+++ DJANGO/django_learning/journal/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login(request):
+    pass
+@login_required
+def journal_entries(request):
+    pass
+@login_required
+def entry_details(request, pk):
+    pass
