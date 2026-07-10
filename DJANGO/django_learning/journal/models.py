f2c32f6583cd5c2157e311c4d566ef9b008908ee
Oscar Somba <maingioscar2@gmail.com>
2026-07-10 09:48:40 +0300

    Created Entry model and all the necessary fields

--- DJANGO/django_learning/journal/models.py
+++ DJANGO/django_learning/journal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Entry(models.Model):
+    entry_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    entry_info = models.CharField(max_length=500)
+    created_At = models.DateTimeField(auto_now_add=True)
