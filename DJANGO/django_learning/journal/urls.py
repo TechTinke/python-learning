9caa0ae9323dba0b85334357d333ba58a8e95394
Oscar Somba <maingioscar2@gmail.com>
2026-07-13 10:40:31 +0300

    Changed the path for entry detail

--- DJANGO/django_learning/journal/urls.py
+++ DJANGO/django_learning/journal/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.login, name='login'),
-    path('/journal_entires', views.journal_entries, name='journal_entries'),
-    path('entry/<str:pk>', views.entry_details, name='entry_details')
+    path('journal_entires', views.journal_entries, name='journal_entries'),
+    path('create_entry', views.create_entry, name='create_entry'),
+    path('entry/<str:pk>', views.entry_detail, name='entry_detail')
 ]
