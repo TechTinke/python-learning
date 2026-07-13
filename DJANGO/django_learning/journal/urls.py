98b4f2ebfec63c69b8fb48191219077e456df6bf
Oscar Somba <maingioscar2@gmail.com>
2026-07-13 10:44:49 +0300

    Final URL changes to journal_entries path

--- DJANGO/django_learning/journal/urls.py
+++ DJANGO/django_learning/journal/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.login, name='login'),
-    path('journal_entires', views.journal_entries, name='journal_entries'),
+    path('journal_entries', views.journal_entries, name='journal_entries'),
     path('create_entry', views.create_entry, name='create_entry'),
     path('entry/<str:pk>', views.entry_detail, name='entry_detail')
 ]
