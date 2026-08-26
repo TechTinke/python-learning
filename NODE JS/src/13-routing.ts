8d024621e80fd0f98de852217b3538d7c4cf4616
Oscar Somba <maingioscar2@gmail.com>
2026-08-26 20:57:18 +0300

    Routing

---
+++ NODE JS/src/13-routing.ts
@@ -0,0 +1,24 @@
+import http from "node:http"
+
+const PORT = 5000
+
+const server = http.createServer((req:IncomingMessage, res:ServerResponse)=>{
+    const method = req.method ?? "GET"
+    const requestUrl = new URL(req.url ?? "/", `http:${req.headers.host}`)
+    const pathName = requestUrl.pathname
+
+    res.setHeader("Content-Type", "text/plain")
+
+    if(method === "GET" && pathName === "/health"){
+        res.statusCode = 200
+        res.end("Server is healthy; up and running")
+        return
+    }
+    res.statusCode = 404
+    res.end("Route not found")
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server is now running on ${PORT}`);
+    
+})
