8f3c4ec0f862c1014ed76082ef0c2a46c85c8be9
Oscar Somba <maingioscar2@gmail.com>
2026-08-27 09:27:29 +0300

    Routing

--- NODE JS/src/13-routing.ts
+++ NODE JS/src/13-routing.ts
@@ -1,24 +1,25 @@
-import http from "node:http"
+import http, { IncomingMessage, ServerResponse } from "node:http";
 
-const PORT = 5000
+const PORT = 5000;
 
-const server = http.createServer((req:IncomingMessage, res:ServerResponse)=>{
-    const method = req.method ?? "GET"
-    const requestUrl = new URL(req.url ?? "/", `http:${req.headers.host}`)
-    const pathName = requestUrl.pathname
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const method = req.method ?? "GET";
+    const requestUrl = new URL(req.url ?? "/", `http:${req.headers.host}`);
+    const pathName = requestUrl.pathname;
 
-    res.setHeader("Content-Type", "text/plain")
+    res.setHeader("Content-Type", "text/plain");
 
-    if(method === "GET" && pathName === "/health"){
-        res.statusCode = 200
-        res.end("Server is healthy; up and running")
-        return
+    if (method === "GET" && pathName === "/health") {
+      res.statusCode = 200;
+      res.end("Server is healthy; up and running");
+      return;
     }
-    res.statusCode = 404
-    res.end("Route not found")
-})
+    res.statusCode = 404;
+    res.end("Route not found");
+  },
+);
 
-server.listen(PORT, ()=>{
-    console.log(`Server is now running on ${PORT}`);
-    
-})
+server.listen(PORT, () => {
+  console.log(`Server is now running on ${PORT}`);
+});
