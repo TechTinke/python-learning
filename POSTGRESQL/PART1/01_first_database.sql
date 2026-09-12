35205a9934448ff2d2bbf4d42e874db5ef142adf
Oscar Somba <maingioscar2@gmail.com>
2026-09-12 15:02:12 +0300

    Fixed the error during dropping and creating database

--- POSTGRESQL/PART1/01_first_database.sql
+++ POSTGRESQL/PART1/01_first_database.sql
@@ -1,15 +1,17 @@
 -- Dropping a database completely removes the database
 -- It's risky to do this during production as you are dealing with real data
--- DROP DATABASE IF EXISTS postgresql_part1
+DROP DATABASE IF EXISTS postgresql_part1;
 
 -- Creating a new database inside postgresql server
-CREATE DATABASE postgresql_part1
+CREATE DATABASE postgresql_part1;
 
 -- psql -U postgres -d postgres -f PART 1/01_first_database.sql
 -- -> Use psql - PostgreSQL
 -- -> Login as postgres user
 -- -> Connect to postgres database
--- -> Run the SQL - PART 1/01_first_database.sql
+-- -> Run the SQL file- PART 1/01_first_database.sql
+
+-- sudo -u postgres psql -d postgres -f PART1/01_first_database.sql
 
 -- PEER AUTHENTICATION - security method that verifies the identity of a connecting user by checking system-level attributes rather than requiring a password or token
 -- sudo -u postgres psql -d postgres -f PART1/01_first_database.sql 
